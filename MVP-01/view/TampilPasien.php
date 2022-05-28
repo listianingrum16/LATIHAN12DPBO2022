dec4500e5b203a82fe8c75b4382045354e77fef5
Listia Ningrum <95666745+listianingrum16@users.noreply.github.com>
2022-05-28 22:50:33 +0700

    Add files via upload

---
+++ MVP-01/view/TampilPasien.php
@@ -0,0 +1,51 @@
+<?php
+
+include("KontrakView.php");
+include("presenter/ProsesPasien.php");
+
+class TampilPasien implements KontrakView
+{
+	private $prosespasien; //presenter yang dapat berinteraksi langsung dengan view
+	private $tpl;
+
+	function __construct(){
+		//konstruktor
+		$this->prosespasien = new ProsesPasien();
+	}
+
+	function tampil(){
+		$this->prosespasien->prosesDataPasien();
+		$data = null;
+
+		//semua terkait tampilan adalah tanggung jawab view
+		for ($i = 0; $i < $this->prosespasien->getSize(); $i++) {
+			$no = $i + 1;
+			$data .= "<tr>
+			<td>" . $no . "</td>
+			<td>" . $this->prosespasien->getNik($i) . "</td>
+			<td>" . $this->prosespasien->getNama($i) . "</td>
+			<td>" . $this->prosespasien->getTempat($i) . "</td>
+			<td>" . $this->prosespasien->getTl($i) . "</td>
+			<td>" . $this->prosespasien->getGender($i) . "</td>
+			<td>" . $this->prosespasien->getEmail($i) . "</td>
+			<td>" . $this->prosespasien->getTelp($i) . "</td>
+			<td scope='row' class='text-center'>
+				<a style='width:80px' href='form-update.php?id_update=".$this->prosespasien->getId($i)."'>
+					<button type='button' class='btn btn-primary  text-white'>Update</button>
+				</a>
+                <a style='width:80px' href='action.php?id_delete=".$this->prosespasien->getId($i)."'>
+					<button type='button' class='btn btn-danger  text-white'>Delete</button>
+				</a>
+            </td>
+			</tr>";
+		}
+		// Membaca template skin.html
+		$this->tpl = new Template("templates/skin.html");
+
+		// Mengganti kode Data_Tabel dengan data yang sudah diproses
+		$this->tpl->replace("DATA_TABEL", $data);
+
+		// Menampilkan ke layar
+		$this->tpl->write();
+	}
+}
